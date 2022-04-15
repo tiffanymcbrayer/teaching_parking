650eec3e9f6d9701d66509969d47512c12d46112
wang-shihao <sw3567@columbia.edu>
2022-04-15 15:47:14 -0400

    Updated navbar with dropdown on hover

--- static/main.js
+++ static/main.js
@@ -1,3 +1,38 @@
-$document.ready(function(){
+// Dropdown on hover
+document.addEventListener("DOMContentLoaded", function () {
+    // make it as accordion for smaller screens
+    if (window.innerWidth > 992) {
+
+        document.querySelectorAll('.navbar .nav-item').forEach(function (item) {
+
+            item.addEventListener('mouseover', function (e) {
+
+                let el_link = this.querySelector('a[data-bs-toggle]');
+
+                if (el_link != null) {
+                    let nextEl = el_link.nextElementSibling;
+                    el_link.classList.add('show');
+                    nextEl.classList.add('show');
+                }
+
+            });
+            item.addEventListener('mouseleave', function (e) {
+                let el_link = this.querySelector('a[data-bs-toggle]');
+
+                if (el_link != null) {
+                    let nextEl = el_link.nextElementSibling;
+                    el_link.classList.remove('show');
+                    nextEl.classList.remove('show');
+                }
+
+            })
+        });
+
+    }
+});
+
+
+
+$document.ready(function () {
 
 })

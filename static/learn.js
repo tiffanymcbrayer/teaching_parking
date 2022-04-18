6de7aa89d09b96733ad8a164450df3b6d17cf50f
Avraham-Kaminker <ark2218@columbia.edu>
2022-04-18 06:57:38 -0700

    added function so buttons change color on hovering

--- static/learn.js
+++ static/learn.js
@@ -10,32 +10,48 @@ var dict = {
     3: "D"
 };
 
+
+function hoverColor(button){
+    $(button).mouseover(function(){
+        button.css('background-color', '#77b5fe'); //how to change back after?
+    });
+    button.mouseout(function(){
+        button.css('background-color', '#007bff'); //how to change back after?
+    });
+}
+
+
 $(document).ready(function () {
     // let ordnumi = int(ordnum)
     if (ordnum == 0) {
-        $('.prev').append("<button id='home'>Home</button>")
+        $('.prev').append("<button class = 'learnButtons' id='home'>Home</button>")
         $(document).on('click', '#home', function (e) {
             window.location.href = '/'
-        });
+        }); 
+        hoverColor($("#home"))
+
     } else {
-        $('.prev').append("<button id='prevtutorial'>Previous</button>")
+        $('.prev').append("<button class = 'learnButtons' id='prevtutorial'>Previous</button>")
         $(document).on('click', '#prevtutorial', function (e) {
             var nextnum = parseInt(ordnum) - 1
             window.location.href = '/learn/' + nextnum
         });
+        hoverColor($("#prevtutorial"))
     }
 
 
     if (ordnum == 3) {
-        $('.next').append("<button id='quiz'>Start Quiz</button>")
+        $('.next').append("<button class = 'learnButtons' id='quiz'>Start Quiz</button>")
         $(document).on('click', '#quiz', function (e) {
             window.location.href = '/quiz/0'
         });
+        hoverColor($("#quiz"))
     } else {
-        $('.next').append("<button id='nexttutorial'>Next</button>")
+        $('.next').append("<button class = 'learnButtons' id='nexttutorial'>Next</button>")
         $(document).on('click', '#nexttutorial', function (e) {
             var nextnum = parseInt(ordnum) + 1
             window.location.href = '/learn/' + nextnum
         });
+        hoverColor($("#nexttutorial"))
     }
 })
